468ccebfd972809c1bc4595603bb27597d17f9a1
ShyamGuna77 <gshyamprasad2001@gmail.com>
2025-09-14 23:16:07 +0530

    fix

---
+++ src/app/svg/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { SVG } from '@/components/svgs/rocket-svg'
+const SvgAnimation = () => {
+  return (
+    <div className=''>
+        <SVG />
+    </div>
+  )
+}
+
+export default SvgAnimation
